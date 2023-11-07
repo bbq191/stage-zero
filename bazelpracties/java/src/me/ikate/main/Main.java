a9cef83e994fd7e3eff109976414f3e2ea47886e
Vinci Xu <xsiyuan@gmail.com>
2023-11-07 15:24:41 +0800

    add: java maven

--- bazelpracties/java/src/me/ikate/main/Main.java
+++ bazelpracties/java/src/me/ikate/main/Main.java
@@ -1,9 +1,13 @@
 package me.ikate.main;
 
+import me.ikate.greeter.Greeter;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bazel packaged");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet());
     }
 
 }
