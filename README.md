adac6000a9aa9de72f348e183c789ff4e1e246da
Vinci Xu <xsiyuan@gmail.com>
2023-11-07 09:07:14 +0800

    fix: move directory

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# stage-zero
+# 自学 ToolChain
+
+1. rust
+2. C++
+3. TypeScript
+4. Bazel
---
+++ bazelpracties/java/src/me/ikate/main/Main.java
@@ -0,0 +1,9 @@
+package me.ikate.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bazel packaged");
+    }
+
+}
