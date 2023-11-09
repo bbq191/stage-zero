d09869b7a2a04f03b890d95daef00e5882de4934
Vinci Xu <xsiyuan@gmail.com>
2023-11-09 13:35:59 +0800

    add: clang-format

--- cxxpraties/basics/main.cpp
+++ cxxpraties/basics/main.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-int sum() { return 1; }
-int sum(int a) { return a; }
-int sum(int a, int b) { return a + b; }
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[]) {
   int a = 100;
-  std::cout << sum(10) << std::endl;
-  std::cout << sum(1, 5) << std::endl;
+  int b = 200;
+  int c = a + b;
   return 0;
 }
