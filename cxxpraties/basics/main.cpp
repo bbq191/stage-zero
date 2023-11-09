5496fd36daf3e1dc632a7948c81adbbfa7f2a89c
Vinci Xu <xsiyuan@gmail.com>
2023-11-09 10:31:51 +0800

    add: asm check

--- cxxpraties/basics/main.cpp
+++ cxxpraties/basics/main.cpp
@@ -3,7 +3,8 @@ int sum() { return 1; }
 int sum(int a) { return a; }
 int sum(int a, int b) { return a + b; }
 int main(int argc, const char *argv[]) {
+  int a = 100;
   std::cout << sum(10) << std::endl;
-  std::cout << sum(1, 5);
+  std::cout << sum(1, 5) << std::endl;
   return 0;
 }
