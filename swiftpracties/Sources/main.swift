ffc12a0b185d5ff82757482ef921d9fc121e4c6e
Vinci Xu <xsiyuan@gmail.com>
2023-11-14 17:52:00 +0800

    mv: swift bazel build

---
+++ swiftpracties/Sources/main.swift
@@ -0,0 +1,7 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+enum Season:Int {case spring=1, summer, autunm, winter}
+var s = Season.autunm
+var r = s.rawValue
+
+print("Hello, world!")
