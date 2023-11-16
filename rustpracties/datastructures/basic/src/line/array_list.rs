85b6b809f984b8d16c32dcc35aa4be8a4924c1cd
Vinci Xu <xsiyuan@gmail.com>
2023-11-16 13:08:04 +0800

    add: datastrutures by rust

---
+++ rustpracties/datastructures/basic/src/line/array_list.rs
@@ -0,0 +1,7 @@
+pub fn add() {
+    println!("quick")
+}
+
+pub fn remove(index: i32) -> bool {
+    return true;
+}
---
+++ rustpracties/datastructures/basic/src/main.rs
@@ -0,0 +1,6 @@
+#[path = "line/mod.rs"]
+mod line;
+fn main() {
+    use crate::line::array_list::*;
+    add();
+}
