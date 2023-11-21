939049a35fd8dc3b5423a730139923d655225619
Vinci Xu <xsiyuan@gmail.com>
2023-11-21 14:42:28 +0800

    add: cmake

--- cxxpraties/basics/main.cpp
+++ cxxpraties/basics/main.cpp
@@ -1,6 +1,25 @@
-int main(int argc, const char* argv[]) {
-  int a = 100;
-  int b = 200;
-  int c = a + b;
+#include <iostream>
+
+struct car
+{
+  int m_price;
+  void run()
+  {
+    this->m_price = 3;
+    // std::cout << "price " << m_price << " car run away" << std::endl;
+  }
+};
+
+int main(int argc, const char *argv[])
+{
+  car a;
+  a.m_price = 1;
+  a.run();
+
+  car b;
+  b.m_price = 2;
+  b.run();
+
+  std::cout << sizeof(a) << sizeof(b) << std::endl;
   return 0;
 }
